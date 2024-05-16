f52b5dedb27470bfbfe9a5727d013981ca1bb5ba
Nathan Ladwig <netham45@gmail.com>
2024-05-16 02:15:39 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#pyScreamReader
+
+This is a Python-based Scream -> PyAudio reader/writer. It's about as basic as possible and is not thoroughly tested.
+
+Requirements: numpy, pyaudio
---
+++ pyscreamreader.py
@@ -0,0 +1,58 @@
+from typing import Optional
+import pyaudio
+import socket
+from threading import Thread
+from scream_header_parser import ScreamHeader
+
+frames = []
+stream: Optional[pyaudio.Stream] = None
+
+def udpStream() -> None:
+    global stream
+    p = pyaudio.PyAudio()
+    header: ScreamHeader = ScreamHeader(bytes([1,32,2,0,0]))
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.bind(("", 4010))
+    while True:
+        soundData, _ = udp.recvfrom(1157)
+        packet_header: ScreamHeader = ScreamHeader(soundData[:5])
+        if packet_header != header:
+            print(f"Got new header {packet_header.bit_depth} {packet_header.sample_rate} {packet_header.channels}")
+            header = packet_header
+            format: int = pyaudio.paInt16
+            if header.bit_depth == 24:
+                format = pyaudio.paInt24
+            elif header.bit_depth == 32:
+                format = pyaudio.paInt32
+            if not stream is None:
+                stream.close()
+            stream = p.open(format=format,
+                            channels = header.channels,
+                            rate = header.sample_rate,
+                            output = True
+                            )
+        frames.append(soundData[5:])
+    udp.close()
+
+def play() -> None:
+    global stream
+    BUFFER = 10
+    while True:
+            if len(frames) >= BUFFER:
+                try:
+                    while True:
+                        if not stream is None:
+                            stream.write(frames.pop(0))
+                        else:
+                            frames.pop(0)
+                except IndexError:  # Buffer is empty, rebuild it
+                    print("Buffer underrun")
+                    pass
+
+if __name__ == "__main__":
+    receiver = Thread(target = udpStream)
+    player = Thread(target = play)
+    receiver.start()
+    player.start()
+    receiver.join()
+    player.join()
