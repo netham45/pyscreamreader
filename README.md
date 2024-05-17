9280271926c7b56b0c46bd65a4b806c8beecd61d
Nathan Ladwig <netham45@gmail.com>
2024-05-16 23:24:47 -0600

    Even smaller

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a Python-based Scream -> PyAudio reader/writer. It's about as basic as possible and is not thoroughly tested.
 
-Requirements: numpy, pyaudio
+Requirements: pyaudio
--- pyscreamreader.py
+++ pyscreamreader.py
@@ -1,62 +1,14 @@
 """Implements PyScreamReader that plays Scream streams using PyAudio"""
-from typing import Optional
-import pyaudio
-import socket
-from threading import Thread
-from scream_header_parser import ScreamHeader
-
-class ScreamReader():
-    """Reads from Scream, writes to PyAudio"""
-    BUFFER: int = 10
-
-    def __init__(self):
-        self.frames = []
-        self.stream: Optional[pyaudio.Stream] = None
-        receiver = Thread(target = self.udpStream)
-        player = Thread(target = self.play)
-        receiver.start()
-        player.start()
-        receiver.join()
-        player.join()
-
-    def udpStream(self) -> None:
-        """This thread receives frames and writes them to a buffer"""
-        p = pyaudio.PyAudio()
-        header: ScreamHeader = ScreamHeader(bytes([1,32,2,0,0]))
-        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp.bind(("", 4010))
-        while True:
-            soundData, _ = udp.recvfrom(1157)
-            packet_header: ScreamHeader = ScreamHeader(soundData[:5])
-            if packet_header != header:
-                print(f"Got new header {packet_header.bit_depth} {packet_header.sample_rate} {packet_header.channels}")
-                header = packet_header
-                if not self.stream is None:
-                    self.stream.close()
-                format: int = pyaudio.paInt16
-                if header.bit_depth == 24:
-                    format = pyaudio.paInt24
-                elif header.bit_depth == 32:
-                    format = pyaudio.paInt32
-                self.stream = p.open(format = format,
-                                     channels = header.channels,
-                                     rate = header.sample_rate,
-                                     output = True
-                                    )
-            self.frames.append(soundData[5:])
-
-    def play(self) -> None:
-        """This thread plays frames that have been received"""
-        while True:
-            if len(self.frames) >= self.BUFFER:
-                try:
-                    while True:
-                        if not self.stream is None:
-                            self.stream.write(self.frames.pop(0))
-                        else:
-                            self.frames.pop(0)
-                except IndexError:  # Buffer is empty, rebuild it
-                    print("Buffer underrun")
-                    pass
-
-ScreamReader()
+from socket import socket, AF_INET, SOCK_DGRAM
+from pyaudio import PyAudio, Stream, paInt16, paInt24, paInt32
+stream: Stream | None = None
+active_header: bytes | None = None
+udp: socket = socket(AF_INET, SOCK_DGRAM)
+udp.bind(("", 4010))
+while True:
+    pcm: bytes = udp.recvfrom(1157)[0]
+    if active_header != pcm[:5]:
+        stream = PyAudio().open(rate=(44100 if (pcm[0] >> 7) else 48000) * (pcm[0] & 127),
+            channels=pcm[2], format={16: paInt16, 24: paInt24, 32: paInt32}[pcm[1]], output=True)
+        active_header = pcm[:5]
+    stream.write(pcm[5:])  # type: ignore
