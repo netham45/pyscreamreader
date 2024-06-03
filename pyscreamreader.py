a41af634b544190e95b01450158910a9ee3142e9
Nathan Ladwig <netham45@gmail.com>
2024-06-03 17:31:06 -0600

    Add small buffer

--- pyscreamreader.py
+++ pyscreamreader.py
@@ -1,14 +1,61 @@
-"""Implements PyScreamReader that plays Scream streams using PyAudio"""
-from socket import socket, AF_INET, SOCK_DGRAM
-from pyaudio import PyAudio, Stream, paInt16, paInt24, paInt32
-stream: Stream | None = None
-active_header: bytes | None = None
-udp: socket = socket(AF_INET, SOCK_DGRAM)
-udp.bind(("", 4010))
-while True:
-    pcm: bytes = udp.recvfrom(1157)[0]
-    if active_header != pcm[:5]:
-        stream = PyAudio().open(rate=(44100 if (pcm[0] >> 7) else 48000) * (pcm[0] & 127),
-            channels=pcm[2], format={16: paInt16, 24: paInt24, 32: paInt32}[pcm[1]], output=True)
-        active_header = pcm[:5]
-    stream.write(pcm[5:])  # type: ignore
+from typing import Optional
+import pyaudio
+import socket
+from threading import Thread
+from scream_header_parser import ScreamHeader
+import time
+
+frames = []
+stream: Optional[pyaudio.Stream] = None
+
+def udpStream() -> None:
+    global stream
+    p = pyaudio.PyAudio()
+    header: ScreamHeader = ScreamHeader(bytes([1,32,2,0,0]))
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.bind(("", 4010))
+    while True:
+        soundData, _ = udp.recvfrom(1157)
+        packet_header: ScreamHeader = ScreamHeader(soundData[:5])
+        if packet_header != header:
+            print(soundData[:5])
+            print(f"Got new header {packet_header.bit_depth} {packet_header.sample_rate} {packet_header.channels}")
+            header = packet_header
+            format: int = pyaudio.paInt16
+            if header.bit_depth == 24:
+                format = pyaudio.paInt24
+            elif header.bit_depth == 32:
+                format = pyaudio.paInt32
+            if not stream is None:
+                stream.close()
+            stream = p.open(format=format,
+                            channels = header.channels,
+                            rate = header.sample_rate,
+                            output = True
+                            )
+        frames.append(soundData[5:])
+    udp.close()
+
+def play() -> None:
+    global stream
+    BUFFER = 5
+    while True:
+        if len(frames) >= BUFFER:
+            try:
+                while True:
+                    if not stream is None:
+                        stream.write(frames.pop(0))
+                    else:
+                        frames.pop(0)
+            except IndexError:  # Buffer is empty, rebuild it
+                print("Buffer underrun")
+        else:
+            time.sleep(.05)
+
+if __name__ == "__main__":
+    receiver = Thread(target = udpStream)
+    player = Thread(target = play)
+    receiver.start()
+    player.start()
+    receiver.join()
+    player.join()
